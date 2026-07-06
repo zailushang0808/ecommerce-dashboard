f6d104b379ce64a964665a60d6212f24497c4e66
Zheng <z@Mac.lan>
2026-07-06 19:43:51 +0800

    Add base config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: '0.0.0.0',
     hmr: false,
